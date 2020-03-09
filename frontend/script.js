a2453c1f89f7c02d06d8668369ad30f0b0c4c46b
Michael <kofronmichael@gmail.com>
2020-03-09 11:14:03 -0500

    animations on buttons

--- frontend/script.js
+++ frontend/script.js
@@ -15,32 +15,47 @@ class Key {
 
 }
 
-if (window.location.href !== 'http://localhost:8080/'){
-    console.log("what")
-}
+let listenerLibrary = {
 
-console.log("hello")
+    searchIconClick: function(){
+        document.getElementById("search").addEventListener("click", function(){
+            let originalContent = document.getElementsByClassName("flex-container")
+            let searchMainDiv = document.getElementsByClassName("search")
+            let searchButton = document.getElementById("search-icon")
+            if (searchMainDiv[0].style.display == "flex"){
+                searchMainDiv[0].style.display = "none"
+            } else {
+                searchButton.style.display = "none"
+                originalContent[0].setAttribute("style", "animation: 0.1s ease-out 0s 1 slideDown")
+                searchMainDiv[0].style.display = "flex"
+            }
+        })
+    },
+
+    loginIconClick: function(){
+        document.getElementById("login").addEventListener("click", function(){
+            let loginMainDiv = document.getElementsByClassName("login")
+            let loginButton = document.getElementById("login-icon")
+            if (loginMainDiv[0].style.display == "flex"){
+                loginMainDiv[0].style.display = "none"
+            } else {
+                loginButton.style.display = "none"
+                loginMainDiv[0].style.display = "flex "
+            }
+        })
+    }
+}
 
 document.addEventListener("DOMContentLoaded", (events) => {
     let key = new Key
     key.key
+
+    listenerLibrary.loginIconClick()
+    listenerLibrary.searchIconClick()
     
     document.getElementById("new-key").addEventListener("click", function(){
         if (confirm("If you change your key you will lose all current tracking information. Continue?")){
             key.key
         }
     })
-
-    document.getElementById("search").addEventListener("click", function(){
-        let searchButton = document.getElementsByClassName("flex-container")
-        let searchMainDiv = document.getElementsByClassName("search")
-        let searchSubDiv = document.getElementById("box")
-        searchButton[0].style.display = "none"
-        searchMainDiv[0].style.display = "flex"
-
-
-    })
-
-    //only do this if window.location.href is the blank route
-    //else we need to find all data and load it
 })
