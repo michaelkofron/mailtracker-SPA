aadecd87b34e035e1bf5dad664c1e908ae15c78f
Michael <kofronmichael@gmail.com>
2020-03-18 16:30:18 -0500

    messageBar fix

--- mail_tracker_api/app/models/number.rb
+++ mail_tracker_api/app/models/number.rb
@@ -1,5 +1,6 @@
 class Number < ApplicationRecord
     belongs_to :user
 
-    validates :number, :presence => true
+    validates :number, :presence => true, :format => { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
+
 end
