6dd03f095c9c49e96d786b3ca5439c0573278922
Michael Kofron <kofronmichael@gmail.com>
2020-08-28 18:32:16 -0500

    api change

--- mail_tracker_api/app/models/key.rb
+++
@@ -1,5 +0,0 @@
-class Key
-    def self.key
-        Rails.application.credentials.dig(:map_key)
-    end
-end
--- mail_tracker_api/db/migrate/20200314231739_create_numbers.rb
+++
@@ -1,8 +0,0 @@
-class CreateNumbers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :numbers do |t|
-      t.string :number
-      t.integer :user_id
-    end
-  end
-end
